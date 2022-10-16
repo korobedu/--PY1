4c12178ae87a785b66eda67bd574470d04a6198f
korobedu <115357300+korobedu@users.noreply.github.com>
2022-10-16 17:17:41 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task1.py
@@ -0,0 +1,9 @@
+src = not False and True or False and not True
+
+# result = not False or False
+# result = not False
+# result = True
+
+result = True
+
+print(src == result)
---
+++ Лабораторная работа 3/task9.py
@@ -0,0 +1,18 @@
+def calculate_pillow(wage, expenses, months, inflation):
+    pillow = 0
+    for month in range(1, months + 1):
+        pillow += wage - expenses
+        expenses *= 1 + inflation
+    # Мы в минусе на pillow рублей
+    # Тогда чтобы прожить 10 месяцев нам нужно минимум (-1)*pillow рублей
+    return (-1) * pillow
+
+
+salary = 5000  # зарплата
+spend = 6000  # траты
+months = 10  # количество месяцев
+increase = 0.03  # рост цен
+
+need_money = calculate_pillow(salary, spend, months, increase)
+
+print(round(need_money))
